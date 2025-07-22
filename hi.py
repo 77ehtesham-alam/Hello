cb73ea9c1c593dd377310a68aed0d12809c9b815
77ehtesham-alam <ehtesham.alam@apphelix.ai>
2025-07-22 12:41:25 +0530

    Part of: Fix code issues in PR #151
    Fixes in hi.py

--- hi.py
+++ hi.py
@@ -1,8 +1,7 @@
-# a
-# a"# a"# a"# aS# ai# am# ap# al# ae# a # as# ac# ar# ai# ap# at# a # at# ao# a # ap# ar# ai# an# at# a # ag# ar# ae# ae# at# ai# an# ag# as# a.# a"# a"# a"# a
-# a
-# ap# ar# ai# an# at# a(# a"# ah# ai# ai# a # aw# ao# ar# al# ad# a"# a)# a
-# ap# ar# ai# an# at# a(# a"# ah# ai# ai# ai# a"# a)# a
-# aX# a # a=# a # a1# a
-# a## a # a## a # a## a # ab# a
-# a
+
+print("hii world")
+print("hiii")
+X = 1
+b b = 2
+d = 3
+print(d)
